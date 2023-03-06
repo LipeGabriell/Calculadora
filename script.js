5fa279a8bdec83279c28d31fa78239c5c55be6dc
Felipe Gabriel <felipe.ramiressanches@hotmail.com>
2023-03-06 11:58:54 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,102 +1,277 @@
 let numAtual = "0";
-var display = document.getElementById("displayOutput");
-let numOperacao = null;
-let wichOperacao = null;
+let numAntes = 0;
+let result;
 
+var display = document.getElementById("displayOutput");
+var displayOperation = document.getElementById("displayOperation");
+var displayNumAbove = document.getElementById("displayAbove");
+
+let numOperacao;
+let wichOperacao;
+
+function setNumAntes() {
+    numAntes = numAtual;
+    numAtual = "0";
+    refreshDisplay();
+    refreshAbove();
+}
+
+function equalReset(resultado) {
+    numAntes = "";
+    numAtual = "0";
+    refreshAbove()
+    refreshOperator("")
+    display.innerText = resultado;
+}
 
 function refreshDisplay() {
     display.innerText = numAtual;
+
 }
+
+function refreshAbove() {
+    displayNumAbove.innerText = numAntes;
+}
+
+function refreshOperator(op) {
+    displayOperation.innerText = op;
+}
+
+function equalOperation() {
+    switch (displayOperation.innerText) {
+        case "+":
+            result = parseInt(numAntes) + parseInt(numAtual);
+            equalReset(result);
+            return;
+        case "-":
+            result = parseInt(numAntes) - parseInt(numAtual);
+            equalReset(result);
+            return;
+        case "*":
+            result = parseInt(numAntes) * parseInt(numAtual);
+            equalReset(result);
+            return;
+        case "/":
+            result = parseInt(numAntes) / parseInt(numAtual);
+            equalReset(result);
+            return;
+    }
+}
+
+function operationDisplay(qualOperacao) {
+
+    switch (qualOperacao) {
+        case "+":
+            refreshOperator("+");
+            if (numAntes == 0) return setNumAntes();
+            else {
+                numAntes = parseInt(numAntes) + parseInt(numAtual);
+                numAtual = "0";
+                refreshDisplay();
+                refreshAbove();
+                return;
+            }
+        case "-":
+            refreshOperator("-")
+            if (numAntes == 0) return setNumAntes();
+            else {
+                numAntes = parseInt(numAntes) - parseInt(numAtual);
+                numAtual = "0";
+                refreshDisplay();
+                refreshAbove();
+                return;
+            }
+
+        case "*":
+            refreshOperator("*")
+            if (numAntes == 0) return setNumAntes();
+            else {
+                numAntes = parseInt(numAntes) * parseInt(numAtual);
+                numAtual = "0";
+                refreshDisplay();
+                refreshAbove();
+                return;
+            }
+
+        case "/":
+            refreshOperator("/")
+            if (numAntes == 0) return setNumAntes();
+            else {
+                numAntes = parseInt(numAntes) / parseInt(numAtual);
+                numAtual = "0";
+                refreshDisplay();
+                refreshAbove();
+                return;
+            }
+
+        default:
+            break;
+    }
+
+
+
+}
+
 
 document.querySelectorAll("div.button").forEach(button => {
     button.addEventListener("click", event => {
         qualButton = button.getAttribute("id");
         switch (qualButton) {
             case "num1":
-                if (numAtual == "0") return numAtual = "1";
+                if (numAtual == "0") {
+                    numAtual = "1"
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 1;
                     refreshDisplay();
+                    return numAtual;
                 }
-                break;
             case "num2":
-                if (numAtual == "0") return numAtual = "2";
+                if (numAtual == "0") {
+                    numAtual = "2"
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 2;
                     refreshDisplay();
+                    return numAtual;
+
                 }
-                break;
             case "num3":
-                if (numAtual == "0") return numAtual = "3";
+                if (numAtual == "0") {
+                    numAtual = "3"
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 3;
                     refreshDisplay();
+                    return numAtual;
+
                 }
-                break;
+
             case "num4":
-                if (numAtual == "0") return numAtual = "4";
+                if (numAtual == "0") {
+                    numAtual = "4"
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 4;
                     refreshDisplay();
+                    return numAtual;
+
                 }
-                break;
+
             case "num5":
-                if (numAtual == "0") return numAtual = "5";
+                if (numAtual == "0") {
+                    numAtual = "5"
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 5;
                     refreshDisplay();
+                    return numAtual;
+
                 }
-                break;
+
             case "num6":
-                if (numAtual == "0") return numAtual = "6";
+                if (numAtual == "0") {
+                    numAtual = "6"
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 6;
                     refreshDisplay();
+                    return numAtual;
+
                 }
-                break;
+
             case "num7":
-                if (numAtual == "0") return numAtual = "7";
+                if (numAtual == "0") {
+                    numAtual = "7"
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 7;
                     refreshDisplay();
+                    return numAtual;
+
                 }
-                break;
+
             case "num8":
-                if (numAtual == "0") return numAtual = "8";
+                if (numAtual == "0") {
+                    numAtual = "8"
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 8;
                     refreshDisplay();
+                    return numAtual;
+
                 }
-                break;
+
             case "num9":
-                if (numAtual == "0") return numAtual = "9";
+                if (numAtual == "0") {
+                    numAtual = "9"
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 9;
                     refreshDisplay();
+                    return numAtual;
+
                 }
-                break;
+
             case "num0":
-                if (numAtual == "0") return numAtual = "0";
+                if (numAtual == "0") {
+                    numAtual = "0";
+                    refreshDisplay();
+                    return numAtual;
+                }
                 else {
                     numAtual = numAtual + 0;
                     refreshDisplay();
+                    return numAtual;
                 }
-                break;
+
             case "soma":
-                console.log("é pra somar");
+                if (numAtual == "0") return;
+                operationDisplay("+");
                 break;
             case "subb":
-                console.log("é pra subtrair");
+                if (numAtual == "0") return;
+                operationDisplay("-");
                 break;
             case "multiply":
-                console.log("é pra multiplicar");
+                if (numAtual == "0") return;
+                operationDisplay("*");
                 break;
             case "division":
-                console.log("é pra dividir");
+                if (numAtual == "0") return;
+                operationDisplay("/");
                 break;
             case "equal":
-                console.log("é pra resolver o q foi pedido");
+                if (numAtual == "0") return;
+                equalOperation();
                 break;
+
+            case "clear":
+                numAtual = "0";
+                numAntes = "";
+                refreshDisplay();
+                refreshAbove("");
+                refreshOperator("")
+                break;
+
             default:
                 alert("erro!")
                 break;
